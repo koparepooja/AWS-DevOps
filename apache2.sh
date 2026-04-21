cc2e06b05a92602be1d06594177d1b022d05b34a
koparepooja <koparepooja@gmail.com>
2026-04-21 09:23:38 +0530

    Cleanup apache2.sh by removing installation commands
    Removed unnecessary commands for installing and starting Apache2.

--- apache2.sh
+++ apache2.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo -i
-apt install apache2 -y
-systemctl start apache2 
-systemctl enable apache2
-echo "Apache2 installed and started succesfully." >/var/www/html/index.html
+echo "Apache2 installed and started succesfully."
